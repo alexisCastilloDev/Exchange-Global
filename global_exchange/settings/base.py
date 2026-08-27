2481becd98cbc75415e8d8abc3dcc7583fc2e5d0
Rodrigo <remm2003@fpuna.edu.py>
2026-08-26 23:50:26 -0300

    Configuración ambientes (AMB)

--- global_exchange/settings/base.py
+++ global_exchange/settings/base.py
@@ -1,6 +1,11 @@
 """
 Configuración base compartida entre dev y prod.
 """
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 from pathlib import Path
 import environ
 
--- global_exchange/settings/prod.py
+++ global_exchange/settings/prod.py
@@ -1,5 +1,18 @@
 from .base import *
 
+from decouple import config
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
+
 DEBUG = False
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
