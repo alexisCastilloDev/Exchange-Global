eb74285c7883a7fb9ad1a447a0a7c8e2663a16bf
alexisCastilloDev <amcs301004@fpuna.edu.py>
2026-09-06 18:20:05 -0300

    Agregando rutas corregidas y autenticacion y authorizacion nueva

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -12,20 +12,3 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
-class RecursoProtegido(models.Model):
-    codigo = models.SlugField(max_length=50, unique=True)
-    nombre = models.CharField(max_length=100)
-
-    def save(self, *args, **kwargs):
-        es_nuevo = self._state.adding
-        super().save(*args, **kwargs)
-        if es_nuevo:
-            content_type = ContentType.objects.get_for_model(RecursoProtegido)
-            Permission.objects.get_or_create(
-                codename=f'acceder_{self.codigo}',
-                content_type=content_type,
-                defaults={'name': f'Puede acceder a {self.nombre}'},
-            )
-
-    def __str__(self):
-        return self.nombre
